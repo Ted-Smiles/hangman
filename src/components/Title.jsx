cdd4b4a5905fe65e2ed4beefb6ac34fe1a8ad627
Ted Smiles <ted@smilesfamily.co.uk>
2024-04-28 15:41:23 +0100

    final production

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,10 @@
 function Title () {
-    return <h1>Hangman</h1>
+    return (
+        <>
+            <h1>Hangman</h1>
+            <h3>By Ted Smiles</h3>
+        </>
+    )
 }
 
 export default Title
